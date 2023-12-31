0294d70732732cdbb097e02326e7fe1dc836fe53
yuvraj singh <yuvrajsingh1704@gmail.com>
2023-12-31 23:01:28 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View className="bg-red-500">
+      <Text className="text-yellow-900">App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
